84cd94b897eb57ae989c0a235d57a770fa13bb73
Alg0v <hugo@algov.ch>
2024-12-22 18:29:56 +0100

    changement taille map + fix

--- src/world/map.cpp
+++ src/world/map.cpp
@@ -3,7 +3,7 @@
 #include <random>
 
 namespace world::map {
-    uint16_t height{4096}, width{4096};
+    uint16_t height{256}, width{256};
     world::Block * * blocks{nullptr};
 
     void init(){
@@ -11,7 +11,8 @@ namespace world::map {
         world::map::blocks = (world::Block * *)malloc(sizeof(Block *) * width * height);
         for(int y = 0; y < height; y++)
             for(int x = 0; x < width; x++)
-                world::map::blocks[y*width + x] = world::blocks[rand() % 5];
+                if(rand() % 8 == 0) world::map::blocks[y*width + x] = world::blocks[1];
+                else world::map::blocks[y*width + x] = world::blocks[0];
     }
 
     void destroy(){
