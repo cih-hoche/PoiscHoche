5221cd86a9937d50cb38a362258562d9fe7933d7
Alg0v <hugo@algov.ch>
2024-12-22 19:34:02 +0100

    début de la classe poisson

--- src/entities/fish.cpp
+++ src/entities/fish.cpp
@@ -1,2 +1,29 @@
 #include "fish.h"
+#include <cmath>
+#include "../world/block.h"
+#define MIN(a,b) (a<=b?a:b)
+#define MAX(a,b) (a<=b?b:a)
 
+namespace entities {
+    std::vector<Fish> fishes;
+
+    void Fish::tick(){
+        x += speed * (float) cos(direction);
+        y += speed * (float) sin(direction);
+
+        bool hasCollidedX{false}, hasCollidedY{false};
+
+        unsigned int x_blocks = (int) MIN(x/BLOCK_HW - 1, 0);
+        unsigned int x_blocks_end = (int) MAX((x + width)/BLOCK_HW + 1, 0);
+        unsigned int y_blocks = (int) MIN(y/BLOCK_HW - 1, 0);
+        unsigned int y_blocks_end = (int) MAX((y + height)/BLOCK_HW + 1, 0);
+        for(unsigned int _y = y_blocks; _y <= y_blocks_end; _y++)
+            for(unsigned int _x = x_blocks; _x <= x_blocks_end; _x++){
+                /* TO FINISH */
+            }
+    }
+
+    Fish::Fish(float _x, float _y, float _speed, float _direction, uint8_t _width, uint8_t _height):
+      x(_x), y(_y), speed(_speed), direction(_direction), width(_width), height(_height){
+    }
+}
--- src/entities/fish.h
+++ src/entities/fish.h
@@ -1,12 +1,24 @@
 #ifndef FISH_H
 #define FISH_H
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <utility>
 
+namespace entities {
+    class Fish {
+    public:
+        float x, y;
+        uint8_t width, height;
+        double direction; //radians
+        float speed;
 
+        void tick(); // appelé à chaque frame
+        Fish(float _x, float _y, float _speed=0.017, float _direction=0, uint8_t _width=32, uint8_t _height=64);
 
-class fish {
+    };
 
-};
-
-
+    extern std::vector<Fish> fishes;
+}
 
 #endif //FISH_H
